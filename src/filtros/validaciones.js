6b8fe722fdbd1c2489a9cff451e0b6c849d75ad0
dylan <dylancotera@gmail.com>
2025-08-30 21:00:46 -0300

    terminado

--- src/filtros/validaciones.js
+++ src/filtros/validaciones.js
@@ -1,1 +1,21 @@
+const reglas = [
+    {
+        fn: "stock",
+        values: (prod,valor) => (prod.stock > valor.min && prod.stock < valor.max) 
+    },
+    {
+        fn: "precio",
+        values: (prod,valor) => (prod.precio <= valor)
+    },
+    {
+        fn: "nombre",
+        values: (prod,valor) => (prod.nombre.length > valor.min && prod.nombre.length < valor.max)
+    },
+    {
+        fn: "categoria",
+        values: (prod,valor) => (valor.includes(prod.categorias))
+    }
+]
 
+
+module.exports = reglas
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+const express = require("express")
+const productos = require("../data/productos.json")
+const reglas = require("./filtros/validaciones")
 
+const app = express()
+app.use(express.json())
+
+app.get("/productos/filtrar", (req,res) => {
+    res.status(200).json(productos)
+})
+
+app.post("/productos/filtrar", (req,res) => {
+    const data = req.body
+    const respuesta = productos.filter(p => data.every(d => {
+        const reglasAUsar = reglas.find(r => r.fn === d.fn)
+        if (reglasAUsar === undefined) return false
+        return reglasAUsar.values(p, d.values)}
+    ))
+    res.status(200).json(respuesta)
+})
+
+app.listen(3000,() => {
+    console.log("Servidor iniciado en puerto 3000")
+})
